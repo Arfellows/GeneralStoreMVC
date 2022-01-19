ac94cb4a3ff46709a6fb6ef538ee28b652bc850f
Ashley Fellows <ashley.r.fellows@gmail.com>
2022-01-18 20:00:48 -0500

    I think I finished transaction?

--- GeneralStoreMVC/Controllers/TransactionController.cs
+++ GeneralStoreMVC/Controllers/TransactionController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GeneralStoreMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,107 @@ namespace GeneralStoreMVC.Controllers
 {
     public class TransactionController : Controller
     {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
         // GET: Transaction
         public ActionResult Index()
         {
+            return View(_db.Transactions.ToList());
+        }
+
+        // GET: Transaction/Create
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        // POST: Transaction/Create
+        [HttpPost]
+        public ActionResult Create(Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Transactions.Add(transaction);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(transaction);
+        }
+
+        // GET: Transaction/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            Transaction transaction = _db.Transactions.Find(id);
+
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
+        }
+
+        // POST: Transaction/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Transaction transaction = _db.Transactions.Find(id);
+            _db.Transactions.Remove(transaction);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET: Transaction/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            Transaction transaction = _db.Transactions.Find(id);
+
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
+        }
+
+        // POST: Transaction/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(transaction).State = System.Data.Entity.EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(transaction);
+        }
+
+        // GET: Transaction/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            Transaction transaction = _db.Transactions.Find(id);
+
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
         }
     }
 }
