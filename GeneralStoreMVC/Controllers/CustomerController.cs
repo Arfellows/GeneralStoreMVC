f2dbf2631abfad78229a13d6ab2ba1c54a7544f2
Ashley Fellows <ashley.r.fellows@gmail.com>
2022-01-18 19:37:06 -0500

    added transaction model

--- GeneralStoreMVC/Controllers/CustomerController.cs
+++ GeneralStoreMVC/Controllers/CustomerController.cs
@@ -14,7 +14,106 @@ namespace GeneralStoreMVC.Controllers
         // GET: Customer
         public ActionResult Index()
         {
+            List<Customer> customerList = _db.Customers.ToList();
+            List<Customer> orderedList = customerList.OrderBy(cust => cust.FirstName).ToList();
+            return View(orderedList);
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Customers.Add(customer);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customer/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            Customer customer = _db.Customers.Find(id);
+
+            if(customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customer/Delete/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Customer customer = _db.Customers.Find(id);
+            _db.Customers.Remove(customer);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Customer/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            Customer customer = _db.Customers.Find(id);
+
+            if(customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customer/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Customer customer)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Entry(customer).State = System.Data.Entity.EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        // GET: Customer/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            Customer customer = _db.Customers.Find(id);
+
+            if(customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
         }
     }
 }
---
+++ GeneralStoreMVC/Models/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStoreMVC.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionID { get; set; }
+     
+        public int Amount { get; set; }
+
+        [ForeignKey("Customer")]
+        public int CustomerID{ get; set; }
+        public virtual Customer Customer { get; set; }
+
+        [ForeignKey("Product")]
+        public int ProductID { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
